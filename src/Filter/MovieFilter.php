b702f26bb6deeb6dc0f50b122da683c4a02bb8a7
TymekB <baran.tymoteusz@gmail.com>
2023-06-16 14:19:17 +0200

    Add getMoviesWithEvenTitleThatStartsWithLetter method to MovieFilter class

--- src/Filter/MovieFilter.php
+++ src/Filter/MovieFilter.php
@@ -16,4 +16,11 @@ class MovieFilter
 
         return $this->movies[$index];
     }
+
+    public function getMoviesWithEvenTitleThatStartsWithLetter(string $letter): array
+    {
+        return array_filter($this->movies, function(string $title) use ($letter) {
+            return str_starts_with($title, $letter) && strlen($title) % 2 === 0;
+        });
+    }
 }
