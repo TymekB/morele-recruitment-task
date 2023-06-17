f76f35fe812504e2c4a8f6ad0665457a117414c0
TymekB <baran.tymoteusz@gmail.com>
2023-06-17 16:51:56 +0200

    Add unit test for checking if AmountGreaterThanMoviesArrayLengthException is thrown when amount is equal zero

--- tests/Unit/MovieFilterTest.php
+++ tests/Unit/MovieFilterTest.php
@@ -5,32 +5,40 @@ declare(strict_types=1);
 namespace App\Tests\Unit;
 
 use App\Filter\Exception\AmountGreaterThanMoviesArrayLengthException;
+use App\Filter\Exception\AmountLowerThanOneException;
 use App\Filter\MovieFilter;
 use App\Provider\MovieProvider;
 use PHPUnit\Framework\TestCase;
 
 class MovieFilterTest extends TestCase
 {
-    private readonly MovieFilter $moviesFilter;
+    private readonly MovieFilter $movieFilter;
 
     protected function setUp(): void
     {
         $moviesProvider = new MovieProvider();
-        $this->moviesFilter = new MovieFilter($moviesProvider->getMovies());
+        $this->movieFilter = new MovieFilter($moviesProvider->getMovies());
     }
 
     public function testIfThreeRandomMoviesAreUnique(): void
     {
-        $randomMovies = $this->moviesFilter->getRandomMovies(3);
+        $randomMovies = $this->movieFilter->getRandomMovies(3);
         $uniqueRandomMovies = array_unique($randomMovies);
 
         $this->assertTrue(count($randomMovies) === count($uniqueRandomMovies));
     }
 
-    public function testIfAmountGreaterThanMoviesArrayLengthExceptionIsThrown()
+    public function testIfAmountGreaterThanMoviesArrayLengthExceptionIsThrown(): void
     {
         $this->expectException(AmountGreaterThanMoviesArrayLengthException::class);
 
-        $this->moviesFilter->getRandomMovies(500);
+        $this->movieFilter->getRandomMovies(500);
+    }
+
+    public function testIfAmountLowerThanOneExceptionIsThrownWhenAmountIsEqualZero(): void
+    {
+        $this->expectException(AmountLowerThanOneException::class);
+
+        $this->movieFilter->getRandomMovies(0);
     }
 }
